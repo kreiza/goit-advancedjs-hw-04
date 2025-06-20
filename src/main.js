a5e518dcbb456847faaaaf5b1dcc80a5d61dc48d
Dmytro Kreiza <dmytro.kreiza@gmail.com>
2025-06-20 14:45:54 -0400

    style changes

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
 
 import { fetchImages } from './js/pixabay-api.js';
 import {
